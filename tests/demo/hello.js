399ef19f241ac6b1a40c534c9ec88d262979ac12
nirviq <nirviq@hotmail.com>
2026-07-22 11:30:35 -0400

    Initial commit

---
+++ tests/demo/hello.js
@@ -0,0 +1,7 @@
+exports.hello = function f1() {
+    return "Hello"
+}
+
+exports.helloworld = function f2() {
+    return "Hello World"
+}
---
+++ tests/my_first_test.spec.js
@@ -0,0 +1,22 @@
+const { test, expect } = require ('@playwright/test');
+// import { test, expect } from '@playwright/test'
+// const { hello, helloworld } = require('./demo/hello.js');
+
+// // MacBook-Pro:Playwright_Automation asifzaman$ node ./tests/my_first_test.spec.js
+// console.log(hello());
+// console.log(helloworld());
+
+// //MacBook-Pro:Playwright_Automation asifzaman$ npx playwright test ./tests/my_first_test.spec.js
+// test('hello test', async () => {
+//   hello();
+// });
+
+// test('helloworld test', async () => {
+//   helloworld();
+// });
+
+test('My first test', async ({page}) => {
+  await page.goto('https://google.com/')
+  await expect(page).toHaveTitle(/Google/);
+  await browser.close();
+})
