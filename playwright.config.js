6cc8a82acc9c3d9e3b1505fdf0ab38f41510b53c
nirviq <nirviq@hotmail.com>
2026-08-06 14:40:55 -0400

    hello

--- playwright.config.js
+++ playwright.config.js
@@ -19,18 +19,21 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  // retries: process.env.CI ? 2 : 0,
+  retries: 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // reporter: 'html'
+  reporter: [['html'],['list'],['line'],['json'],['junit'],['dot'], ['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    // trace: 'on-first-retry',
+    trace: 'retain-on-failure',  
   },
 
   /* Configure projects for major browsers */
--- tests/demo/hello.js
+++ tests/demo/hello.js
@@ -5,3 +5,10 @@ exports.hello = function f1() {
 exports.helloworld = function f2() {
     return "Hello World"
 }
+
+
+import {test, expect} from '@playwright/test';
+
+test('test demo', async ({page}) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('[data-test="username"]').click();
