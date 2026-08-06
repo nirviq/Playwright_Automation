9f10301889f1e2bec93ece73814b3587e6370373
nirviq <nirviq@hotmail.com>
2026-08-06 15:10:43 -0400

    test

--- tests/record1_demo.spec.js
+++ tests/record1_demo.spec.js
@@ -5,5 +5,5 @@ test('record demo test', async ({ page }) => {
   await page.locator('[data-test="username"]').click();
   await page.locator('[data-test="username"]').fill('standard_user');
   await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-buttonASIF1"]').click();
+  await page.locator('[data-test="login-buttonASIF"]').click();
 });
