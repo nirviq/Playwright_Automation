786fe0b6ea0b834b7ebf144480666f7265cfebd8
nirviq <nirviq@hotmail.com>
2026-08-19 10:33:13 -0400

    config and json file update

--- playwright.config.js
+++ playwright.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
   // retries: process.env.CI ? 2 : 0,
   retries: 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html'
   reporter: [['html'],['list'],['line'],['json'],['junit'],['dot'], ['allure-playwright']],
